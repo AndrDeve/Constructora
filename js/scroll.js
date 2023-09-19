4ccd4ededea7c6fff64786d471005750054994a9
Andrés <marlon_rivera95@hotmail.com>
2023-09-19 10:53:58 -0500

    subida de pagina

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal();
+
+    window.sr = ScrollReveal();
+
+    sr.reveal('.scroll-right' , {
+        duration: 2500,
+        origin: 'right',
+        distance: '20px'
+    });
+
+    window.sr = ScrollReveal();
+
+    sr.reveal('.scroll-left' , {
+        duration: 2500,
+        origin: 'left',
+        distance: '20px'
+    });
